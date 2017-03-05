2601de65a0009eb50123a22e60b52263e900877d
Robin Appelman <robin@icewind.nl>
2017-03-05 23:54:44 +0100

    better celt handling

--- src/Parser/Packet/VoiceInit.ts
+++ src/Parser/Packet/VoiceInit.ts
@@ -5,7 +5,7 @@ export function VoiceInit(stream: BitStream): VoiceInitPacket {
 	const codec     = stream.readASCIIString();
 	const quality   = stream.readUint8();
 	// no clue, from 2017-2-14 update
-	const extraData = (codec === 'vaudio_celt') ? stream.readUint16() : 0;
+	const extraData = (codec === 'vaudio_celt' && quality === 255) ? stream.readUint16() : 0;
 	return {
 		packetType: 'voiceInit',
 		codec:      codec,
