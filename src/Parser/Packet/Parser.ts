498065a4c9667b844a25d06a3bdc018cfc0583a2
Robin Appelman <robin@icewind.nl>
2017-03-07 18:35:04 +0100

    fix type definition

--- src/Parser/Packet/Parser.ts
+++ src/Parser/Packet/Parser.ts
@@ -2,5 +2,5 @@ import {Packet} from "../../Data/Packet";
 import {BitStream} from 'bit-buffer';
 import {Match} from "../../Data/Match";
 
-export type Parser = (stream: BitStream, match?: Match, skip: boolean = false) => Packet;
+export type Parser = (stream: BitStream, match?: Match, skip?: boolean) => Packet;
 export type PacketParserMap = {[id: number]: Parser};
