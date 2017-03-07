7692aa61c9b49dc53b1cfd501d97d225e8fb5e38
Robin Appelman <robin@icewind.nl>
2017-03-07 18:17:58 +0100

    cleanup bin

--- bin/analyse.js
+++ bin/analyse.js
@@ -14,15 +14,6 @@ var echo = function (data) {
 	console.log(string);
 };
 
-//var stream = fs.createReadStream(argv._[0]);
-//
-//var demo = Demo.fromNodeStream(stream);
-//var parser = demo.getParser();
-//parser.on('done', function (state) {
-//	echo(state);
-//});
-//parser.start();
-
 fs.readFile(argv._[0], function (err, data) {
 	if (err) throw err;
 	var demo = Demo.fromNodeBuffer(data);
