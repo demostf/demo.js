721cecb6f751eecec385e40fe47fa4ee0c8e8157
Robin Appelman <robin@icewind.nl>
2017-10-24 23:18:26 +0200

    fix parsing pyro update demos

--- src/Parser/Packet/VoiceInit.ts
+++ src/Parser/Packet/VoiceInit.ts
@@ -5,11 +5,22 @@ export function VoiceInit(stream: BitStream): VoiceInitPacket {
 	const codec     = stream.readASCIIString();
 	const quality   = stream.readUint8();
 	// no clue, from 2017-2-14 update
-	const extraData = (codec === 'vaudio_celt' && quality === 255) ? stream.readUint16() : 0;
+	const extraData = readExtraData(stream, codec, quality);
+
 	return {
 		packetType: 'voiceInit',
 		codec:      codec,
 		quality:    quality,
 		extraData:  extraData
 	};
+}
+
+function readExtraData(stream: BitStream, codec: string, quality: number) {
+	if (quality === 255) {
+		return stream.readUint16();
+	} else if (codec === 'vaudio_celt') {
+		return 11025;
+	} else {
+		return 0;
+	}
 }
